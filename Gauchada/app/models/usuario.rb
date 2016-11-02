38f63c9132181fd9a1de6c0dfb9a5ff7aa855df1
Gonzalo J. Poch <gonzalopoch@hotmail.com>
2016-11-02 17:43:45 -0300

    Corrección de comas sobrantes.

--- Gauchada/app/models/usuario.rb
+++ Gauchada/app/models/usuario.rb
@@ -1,10 +1,9 @@
 class Usuario < ActiveRecord::Base
-<<<<<<< HEAD
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :trackable, :validatable
          #:confirmable
 
 	belongs_to :logro
