b29ce8c613277b118f06cda2c520bff93c069793
Rocio Madou <rocio.madou@gmail.com>
2016-11-02 15:58:25 -0300

    Se elimino administrador y modificó usuarios

--- Gauchada/db/migrate/20161102002728_create_usuarios.rb
+++ Gauchada/db/migrate/20161102002728_create_usuarios.rb
@@ -3,12 +3,11 @@ class CreateUsuarios < ActiveRecord::Migration
     create_table :usuarios do |t|
       t.string :nombre
       t.string :apellido
-      t.date :fecha_nacimiento
       t.string :email
+      t.date :fecha_nacimiento
       t.integer :telefono
       t.integer :puntos
       t.string :contrasena
-
       t.timestamps null: false
     end
   end
