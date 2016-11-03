abf6f736f377fdfb96f68d605019d3c45d07056b
ivankras <ivankras@hotmail.com>
2016-11-03 20:37:24 -0300

    -

--- Gauchada/db/schema.rb
+++ Gauchada/db/schema.rb
@@ -59,6 +59,13 @@ ActiveRecord::Schema.define(version: 20161102184935) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "resenia", force: :cascade do |t|
+    t.boolean  "cumplido"
+    t.string   "descripcion"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "usuarios", force: :cascade do |t|
     t.string   "nombre"
     t.string   "apellido"
@@ -69,6 +76,7 @@ ActiveRecord::Schema.define(version: 20161102184935) do
     t.string   "contrasena"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.integer  "logro_id"
     t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -78,7 +86,6 @@ ActiveRecord::Schema.define(version: 20161102184935) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "logro_id"
     t.boolean  "admin",                  default: false
   end
 
