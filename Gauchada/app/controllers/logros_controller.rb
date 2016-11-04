40250b552df1ad221119ec2bc5c58453542d8df6
ivankras <ivankras@hotmail.com>
2016-11-03 23:02:31 -0300

    Algunas modificaciones; interiores a la historia

--- Gauchada/app/controllers/logros_controller.rb
+++ Gauchada/app/controllers/logros_controller.rb
@@ -1,10 +1,30 @@
 class LogrosController < ApplicationController
 	def new
+		@logro=Logro.new()
 	end
 	def edit
 		
 	end
 	def create
+		@logro=Logro.create(params.require(:logro).permit(:nombre, :puntaje_min, :puntaje_max))
+		a=true;
+		b=@logro.puntaje_min;
+		c=@logro.puntaje_max;
+		Logro.each do |l|
+			a=a&&l.out_range(b,c)
+			if (!a)
+				break
+			end
+		end
+		if (!a)
+			#No me cierra ni un poquito. La idea es que si no está validado vuelva a pedir los datos
+			redirect_to @logro.create, :notice => “Un nuevo logro ha sido insertado”;
+		else
+			redirect_to logros_path, :notice => “Un nuevo logro ha sido insertado”;
+		end
+		# Falta enviar mensaje de confirmación
+		# Iría un redirect_to, no sé a dónde
+		# redirect_to logros_path, :notice => “Un nuevo logro ha sido insertado”;
 	end
 	def update
 	end
--- Gauchada/app/models/logro.rb
+++ Gauchada/app/models/logro.rb
@@ -1,6 +1,7 @@
 class Logro < ActiveRecord::Base
 	has_many :usuarios
-
 	default_scope -> { order ("puntaje_max desc")} 
-
+	def out_range (min, max)
+		return (((min<this.puntaje_min)&&(max<this.puntaje_max))||(min>this.puntaje_min)&&(max>this.puntaje_max))
+	end
 end
