3bf150b1088afed9029f63c821de53c806860faf
ivankras <ivankras@hotmail.com>
2016-11-04 08:44:41 -0300

    Gema Bootstrap borrada

--- Gauchada/app/controllers/logros_controller.rb
+++ Gauchada/app/controllers/logros_controller.rb
@@ -7,21 +7,19 @@ class LogrosController < ApplicationController
 		
 	end
 	def create
-		@logro=Logro.create(params.require(:logro).permit(:nombre, :puntaje_min, :puntaje_max))
-		a=true;
-		b=@logro.puntaje_min;
-		c=@logro.puntaje_max;
+		a=true
+		b=params[:logro][:puntaje_min]
+		c=params[:logro][:puntaje_max]
 		Logro.each do |l|
-			a=a&&l.out_range(b,c)
-			if (!a)
-				break
-			end
+			a &&= l.out_range(b,c)
+			break unless a
 		end
-		if (!a)
-			#No me cierra ni un poquito. La idea es que si no está validado vuelva a pedir los datos
-			redirect_to (@logro.create, notice: “Un nuevo logro ha sido insertado”)
+		unless a
+			@logro=Logro.create(params.require(:logro).permit(:nombre, :puntaje_min, :puntaje_max))
+			#La idea es que si no está validado vuelva a pedir los datos
+			redirect_to(new_logro_path, notice: "Un nuevo logro ha sido insertado")
 		else
-			redirect_to (logros_path, notice: “Un nuevo logro ha sido insertado”)
+			redirect_to(logros_path, notice: "Un nuevo logro ha sido insertado")
 		end
 		# Falta enviar mensaje de confirmación
 		# Iría un redirect_to, no sé a dónde
