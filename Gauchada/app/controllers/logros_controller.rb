55f7a3466bb05286900e92aa30f66ec77c1679b2
ivankras <ivankras@hotmail.com>
2016-11-04 19:14:26 -0300

    Vistas de new y edit. Faltan ajustes

--- Gauchada/app/controllers/logros_controller.rb
+++ Gauchada/app/controllers/logros_controller.rb
@@ -16,19 +16,14 @@ class LogrosController < ApplicationController
 	end
 
 	def create
-		a=true
 		b=params[:logro][:puntaje_min]
 		c=params[:logro][:puntaje_max]
-		Logro.each do |l|
-			a &&= l.out_range(b,c)
-			break unless a
-		end
-		unless a
-			@logro=Logro.create(params.require(:logro).permit(:nombre, :puntaje_min, :puntaje_max))
+		unless Logro.verificar_rangos(b,c)
 			#La idea es que si no está validado vuelva a pedir los datos
-			redirect_to(new_logro_path, notice: "Un nuevo logro ha sido insertado")
+			redirect_to(new_logro_path, alert: "Error en el rango elegido. Redefina los limites.")
 		else
-			redirect_to(logros_path, notice: "Un nuevo logro ha sido insertado")
+			@logro=Logro.create(params.require(:logro).permit(:nombre, :puntaje_min, :puntaje_max))
+			redirect_to(logros_path, success: "Un nuevo logro ha sido insertado")
 		end
 		# Falta enviar mensaje de confirmación
 		# Iría un redirect_to, no sé a dónde
@@ -36,6 +31,15 @@ class LogrosController < ApplicationController
 	end
 
 	def update
+		a=params[:logro][:nombre]
+		b=params[:logro][:puntaje_min]
+		c=params[:logro][:puntaje_max]
+		unless Logro.verificar_rangos(b,c)
+			redirect_to(edit_logro_path(@logro.id), alert: "Error en el rango elegido. Redefina los limites.")
+		else
+			@logro=Logro.modificar_datos(a,b,c)
+			redirect_to(logros_path, success: "Un nuevo logro ha sido insertado")
+		end
 	end
 
 	def destroy
--- Gauchada/app/models/logro.rb
+++ Gauchada/app/models/logro.rb
@@ -2,6 +2,19 @@ class Logro < ActiveRecord::Base
 	has_many :usuarios
 	default_scope -> { order ("puntaje_max desc")} 
 	def out_range (min, max)
-		return (((min<this.puntaje_min)&&(max<this.puntaje_max))||(min>this.puntaje_min)&&(max>this.puntaje_max))
+		((min<this.puntaje_min)&&(max<this.puntaje_max))||(min>this.puntaje_min)&&(max>this.puntaje_max)
+	end
+	def self.verificar_rangos(min, max)
+		a=true
+		Logro.each do |l|
+			a &&= l.out_range(min,max)
+			break unless a
+		end
+		a
+	end
+	def self.modificar_datos(nombre, min, max)
+		this.nombre=nombre
+		this.min=min
+		this.max=max
 	end
 end
