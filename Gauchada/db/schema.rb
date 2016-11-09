536f7e1b4c8e105f62488a4f4ef8c01a66330132
Gonzalo J. Poch <gonzalopoch@hotmail.com>
2016-11-09 14:43:24 -0300

    Agrego seeds para favores

--- Gauchada/db/schema.rb
+++ Gauchada/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104131452) do
+ActiveRecord::Schema.define(version: 20161108024901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20161104131452) do
     t.string   "last_sign_in_ip"
     t.integer  "logro_id"
     t.boolean  "admin",                            default: false
+    t.boolean  "sexo",                             default: true
   end
 
   add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
--- Gauchada/db/seeds.rb
+++ Gauchada/db/seeds.rb
@@ -18,3 +18,28 @@ Logro.find_or_create_by(nombre: "Piola", puntaje_min: 11, puntaje_max: 20)
 Logro.find_or_create_by(nombre: "Buena Persona", puntaje_min: 21, puntaje_max: 30)
 Logro.find_or_create_by(nombre: "Capo", puntaje_min: 31, puntaje_max: 100)
 Logro.find_or_create_by(nombre: "Juan Pablo II", puntaje_min: 101, puntaje_max: 2000)
+
+Favor.destroy_all
+Favor.create!([{
+	titulo: "Donald Trump",
+	foto_URL: "https://cdn.theatlantic.com/assets/media/img/2016/05/select_32/hero_wide_640.jpg?1463509000",
+	descripcion: "Donald John Trump was elected the 45th president of the United States on Tuesday in a stunning culmination of an explosive, populist and polarizing campaign that took relentless aim at the institutions and long-held ideals of American democracy.
+
+	The surprise outcome, defying late polls that showed Hillary Clinton with a modest but persistent edge, threatened convulsions throughout the country and the world, where skeptics had watched with alarm as Mr. Trump’s unvarnished overtures to disillusioned voters took hold.
+
+	The triumph for Mr. Trump, 70, a real estate developer-turned-reality television star with no government experience, was a powerful rejection of the establishment forces that had assembled against him, from the world of business to government, and the consensus they had forged on everything from trade to immigration.",
+	ciudad: "New York",
+	},
+	{
+	titulo: "Favor Sin Foto",
+	descripcion: "Dale a tu cuerpo alegria macarena que tu cuerpo es pa darle alegria y cosa buena",
+	ciudad: "Yapeyú",
+	},
+	{
+	titulo: "Favor Prueba",
+	foto_URL: "https://pbs.twimg.com/profile_images/473506797462896640/_M0JJ0v8.png",
+	descripcion: "Esto es una prueba.",
+	ciudad: "Tokio",
+}])
+
+puts "Creo #{Favor.count} favores."
