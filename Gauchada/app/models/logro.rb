1e3cf573b4361811caa88d13c5496767b45cd63c
ivankras <ivankras@hotmail.com>
2016-11-09 16:18:21 -0300

    Algo

--- Gauchada/app/models/logro.rb
+++ Gauchada/app/models/logro.rb
@@ -8,19 +8,19 @@ class Logro < ActiveRecord::Base
 	def rango
     	val = true
     	Logro.all.each do |logro|
-          if val && self.id != logro.id && (self.puntaje_max.between?(logro.puntaje_min,logro.puntaje_max)||self.puntaje_min.between?(logro.puntaje_min,logro.puntaje_max))
-      		#if val && self.id != logro.id && ((self.puntaje_max>=logro.puntaje_min && self.puntaje_max<=logro.puntaje_max)||(self.puntaje_min>=logro.puntaje_min && self.puntaje_min<=logro.puntaje_max))
+          #if val && self.id != logro.id && (self.puntaje_max.between?(logro.puntaje_min,logro.puntaje_max)||self.puntaje_min.between?(logro.puntaje_min,logro.puntaje_max))
+      		if val && id != logro.id && ((puntaje_max>=logro.puntaje_min && puntaje_max<=logro.puntaje_max)||(puntaje_min>=logro.puntaje_min && puntaje_min<=logro.puntaje_max))
         		val = false
       		end
       		break unless val
     	end
-    	errors.add(self.puntaje_min, "Rango incorrecto de valores para el logro.") unless val
+    	errors.add(:puntaje_min, "Rango incorrecto de valores para el logro.") unless val
     	return val
   	end
 
   	def minimoMenorQueMaximo
   		val= self.puntaje_min < self.puntaje_max
-	    if val
+	    unless val
 	      errors.add(:puntaje_min, "Mínimo del logro mayor o igual que máximo; debe ser menor.")
 	  	end
 	  	return val
