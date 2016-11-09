7d1dc23891291e39897c5a8c2c08dd03394459ba
Rocio Madou <rocio.madou@gmail.com>
2016-11-09 19:41:51 -0300

    Eliminados usuarios de seed

--- Gauchada/db/seeds.rb
+++ Gauchada/db/seeds.rb
@@ -5,12 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-puts "Creo usuarios administradores"
-Usuario.find_or_create_by(nombre: "Nancy", apellido: "Netramonti", email: "nancy@gauchada.com", fecha_nacimiento: Date.new(1967,6,16), telefono: 542262106509, password: "Abracadabra", admin: true)
-Usuario.find_or_create_by(nombre: "Ulises", apellido: "Sunetri", email: "ulises@gauchada.com", fecha_nacimiento: Date.new(1963,3,15), telefono: 542211097607, password: "Abracadabra", admin: true)
-Usuario.find_or_create_by(nombre: "Iván", apellido: "Krasowski Bissio", email: "ivankras@hotmail.com", fecha_nacimiento: Date.new(1996,4,30), telefono: 542314401890, password: "Abracadabra")
-Usuario.find_or_create_by(nombre: "Rocío", apellido: "Madou", email: "rocio.madou@hotmail.com", fecha_nacimiento: Date.new(1995,9,27), telefono: 542346571510, password: "Abracadabra")
-Usuario.find_or_create_by(nombre: "Gonzalo Julián", apellido: "Poch", email: "gonzalopoch@hotmail.com", fecha_nacimiento: Date.new(1996,4,29), telefono: 542215794999, password: "Abracadabra")
+
 
 puts "Creo algunos logros"
 Logro.find_or_create_by(nombre: "Promedio", puntaje_min: 0, puntaje_max: 10)
