bfef9a9acae8c639d2fc074601feba7c33a7344c
Rocio Madou <rocio.madou@gmail.com>
2016-11-10 19:55:03 -0300

    Se modificaron vistas

--- Gauchada/db/schema.rb
+++ Gauchada/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109223202) do
+ActiveRecord::Schema.define(version: 20161110041741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20161109223202) do
     t.string   "respuesta"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "favor_id"
   end
 
   create_table "favors", force: :cascade do |t|
