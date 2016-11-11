dcf3af3867f341128307687aff30825c854f6f74
ivankras <ivankras@hotmail.com>
2016-11-10 21:52:09 -0300

    Anda Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,7 @@ Rails.application.configure do
       authentication: "plain",
       enable_starttls_auto: true,
       user_name: "noreply.gauchadaitw@gmail.com",
-      password: "Abracadabra" }
+      password: "Abracadabra" 
+    }
 
 end
