e5bbd9e2f108d24c41104a8cff866e8075cf0ac6
Rocio Madou <rocio.madou@gmail.com>
2016-11-15 00:12:19 -0300

    Tratando de hacer un pull

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,18 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { 
-      address: "smtp.gmail.com",
-      port: 587,
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: "noreply.gauchadaitw@gmail.com",
-      password: "Abracadabra"
-  }
-
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -51,8 +39,23 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  #Mailer configuration
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8" 
+  config.action_mailer.default_url_options = { 
+      address: "smtp.gmail.com",
+      port: 587,
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: "noreply.gauchadaitw@gmail.com",
+      password: "Abracadabra"
+  }
+  
   # Configurar zona horaria
-    config.time_zone = "Buenos Aires"
-    config.active_record.default_timezone = :local
+  config.time_zone = "Buenos Aires"
+  config.active_record.default_timezone = :local
 
 end
