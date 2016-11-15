7321c1bc955f912fefc449e970b1c561373b29ab
ivankras <ivankras@hotmail.com>
2016-11-15 00:49:17 -0300

    Agrego barra lateral. Validación logro perfeccionada

--- app/models/logro.rb
+++ app/models/logro.rb
@@ -6,21 +6,21 @@ class Logro < ActiveRecord::Base
 	validate :rango, :minimoMenorQueMaximo
 
 	def rango
-    	val = true
-    	Logro.all.each do |logro|
-        if logro.activo == true
+      minAbs = Logro.where(activo: true).first.puntaje_min
+      maxAbs = Logro.where(activo: true).first.puntaje_max
+      val = min>minAbs || max<maxAbs
+    	Logro.where(activo: true).each do |logro|
           #pert = (id != logro.id) && (puntaje_max.between?(logro.puntaje_min,logro.puntaje_max)||puntaje_min.between?(logro.puntaje_min,logro.puntaje_max))
       		#pert = ((puntaje_max>=logro.puntaje_min && puntaje_max<=logro.puntaje_max)||(puntaje_min>=logro.puntaje_min && puntaje_min<=logro.puntaje_max))
-          if (id != logro.id)
-            min = puntaje_min
-            max = puntaje_max
-            lmin = logro.puntaje_min
-            lmax = logro.puntaje_max
-            pert = (min >= lmin && min <= lmax) || (max >= lmin && max <= lmax)
-          	val = !pert
-      		end
-        end
-      	break unless val    	   
+        break unless val
+        if (id != logro.id)
+          min = puntaje_min
+          max = puntaje_max
+          lmin = logro.puntaje_min
+          lmax = logro.puntaje_max
+          pert = (min >= lmin && min <= lmax) || (max >= lmin && max <= lmax)
+        	val = !pert
+    		end    	   
       end
     	errors.add(:puntaje_min, "Rango incorrecto de valores para el logro.") unless val
     	return val
