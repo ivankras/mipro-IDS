41725773bf3accb67afc4825cf85fc5da6e23b02
ivankras <ivankras@hotmail.com>
2016-11-16 13:22:28 -0300

    Create de favor, vista de new favor

--- app/controllers/favors_controller.rb
+++ app/controllers/favors_controller.rb
@@ -20,6 +20,15 @@ class FavorsController < ApplicationController
 	end
 
 	def create
+		@favor = Favor.new(params.require(:favor).permit(:titulo, :descripcion, :ciudad, :foto_url))
+	    if @favor.save
+	    	current_usuario.puntos -= 1
+	    	current_usuario.save
+	    	flash[:success] = "Tu favor ha sido publicado"
+	      	redirect_to(logros_path)
+	    else
+	      	redirect_to(new_logro_path, alert: "Error en el título (vacío o muy largo)")
+	    end
 	end
 
 	def update
