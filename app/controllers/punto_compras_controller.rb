1777f6caf802463c183bffad5d521bc30aec934b
Gonzalo J. Poch <gonzalopoch@hotmail.com>
2016-11-17 12:30:18 -0300

    Termino historia de comprar. Quedó diviiiiiiino.

--- app/controllers/punto_compras_controller.rb
+++ app/controllers/punto_compras_controller.rb
@@ -29,15 +29,26 @@ class PuntoComprasController < ApplicationController
 	def agregarpuntos
 		puntos = 0
 		dinero = 0
-		if (params[:cantidad] != nil)
-			dinero +=params[:cantidad].to_i
+		if ((params[:Año] == "2016") && (params[:Mes] != "Diciembre"))
+			flash[:danger] = "La tarjeta ingresada se encuentra vencida."
+			redirect_to(new_punto_compra_path) and return
 		end
-		if (params[:InputPuntos] != nil)
-			puntos +=params[:InputPuntos].to_i
+		if (params[:tarjeta]!="00000000000000") && (params[:tarjeta]!="11111111111111")
+			if (params[:cantidad] != nil)
+				dinero +=params[:cantidad].to_i
+			end
+			if (params[:InputPuntos] != nil)
+				puntos +=params[:InputPuntos].to_i
+			end
+			res = (dinero / 12) + puntos
+			current_usuario.puntos += res.to_i
+			current_usuario.save
+			flash[:success] = "La compra ha sido efectuada."
+		elsif (params[:tarjeta]=="00000000000001")
+			flash[:danger] = "La tarjeta ingresada es inválida."
+		else
+			flash[:danger] = "La tarjeta no dispone de fondos suficientes."
 		end
-		res = (dinero / 12) + puntos
-		current_usuario.puntos += res.to_i
-		current_usuario.save
 		redirect_to(new_punto_compra_path)
 	end
 
